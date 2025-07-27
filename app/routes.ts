41409aaca441ce7babe84ef08011e259513bed1b
Sahil Thakur <sahilthakur.af@gmail.com>
2025-07-28 02:00:10 +0530

    Cloud storage, PDF-Image conversion, Key-value Storage and AI feedback

--- app/routes.ts
+++ app/routes.ts
@@ -1,6 +1,8 @@
 import { type RouteConfig, index, route } from "@react-router/dev/routes";
-import { routes } from "virtual:react-router/server-build";
 
 export default [index("routes/home.tsx"),
    route('/auth',
-     'routes/auth.tsx'),] satisfies RouteConfig;
+     'routes/auth.tsx'),
+    route('/upload',
+      'routes/upload.tsx')
+  ] satisfies RouteConfig;
